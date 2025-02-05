764f2882fe407b9dbb9f862e979b8d44320b22c1
vegaronel <ronelvega31@gmail.com>
2025-02-05 11:11:00 +0800

    change icon to black and white

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -5,10 +5,10 @@ function Skills() {
   return (
     <div className="w-full space-y-4">
       <h2 className="text-base">Skills</h2>
-      <div className="grid grid-cols-2 md:grid-cols-3 gap-4">
+      <div className="grid grid-cols-2 lg:grid-cols-3 gap-4">
         {skillIcons.map((item, index) => (
-          <div key={index} className="flex gap-2 items-center rounded-xl hover:bg-slate-200 dark:hover:bg-slate-800 cursor-pointer duration-300 px-3 py-1 transition-all">
-            <div className="w-[50px] h-[50px]">{item.icon}</div>
+          <div key={index} className="flex  gap-2 items-center rounded-xl hover:bg-slate-200 dark:hover:bg-slate-800 cursor-pointer duration-75 px-3 py-1 transition-all">
+            <div className="w-[50px] h-[50px] ">{item.icon}</div>
             <small className="">{item.name}</small>
           </div>
         ))}
