0adcf761cca91a627ed75fe1190656dfcec71cb2
Ronel Vega <ronelvega31@gmail.com>
2025-11-26 16:33:48 +0800

    ✨Enhance ProjectCard with image gallery functionality

--- src/data/projects.js
+++ src/data/projects.js
@@ -4,6 +4,20 @@ const projects = [
     description:
       "A web application designed to help household families in Brgy. Guinacutan, Vinzons, Camarines Norte. It includes Arduino integration to detect if plants need watering, allows users to add new plants with estimated harvest times, and features a weather API, plant disease detection, and a forum for user communication.",
     technologies: ["Node.js", "Express", "Bootstrap", "EJS", "Arduino"],
+    images: [
+      {
+        image: `/assets/project-seed.svg`,
+        text: "Landing Page",
+      },
+      {
+        image: `/assets/project-seed1.jpg`,
+        text: "Login Page",
+      },
+      {
+        image: `/assets/project-seed2.jpg`,
+        text: "Registration Page",
+      },
+    ],
     github: "https://github.com/vegaronel/permaculture",
     liveDemo: "https://permaculture.onrender.com/", // Add your live demo link here if available
   },
@@ -19,16 +33,10 @@ const projects = [
     name: "Job Compass Tracker",
     description:
       "o	Job Compass is a React web app that centralizes job search management. It connects securely to Gmail via OAuth to ingest and categorize recruiting emails, tracks applications and interviews in a streamlined dashboard with real-time stats, and includes an AI assistant for summarizing threads and drafting responses",
-    technologies: [
-      "Nextjs",
-      "Postgres",
-      "Supabase",
-      "Tailwind CSS",
-      "shadcn",
-    ],
+    technologies: ["Nextjs", "Postgres", "Supabase", "Tailwind CSS", "shadcn"],
     github: "https://github.com/vegaronel/job-compass-career-tracker",
     liveDemo: "https://job-compass-career-tracker.vercel.app/#",
-    status: "Completed"
+    status: "Completed",
   },
 ];
 
